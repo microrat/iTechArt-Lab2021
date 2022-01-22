c5a201570eff0051881819758f484da9bc0fa478
xxpolina <laracroftby@gmail.com>
2022-01-22 11:14:33 +0300

    Added pagination
    Menu.jsx pagination

--- src/components/Home/components/Menu.jsx
+++ src/components/Home/components/Menu.jsx
@@ -1,26 +1,65 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import MenuItem from "./MenuItem.jsx";
 import s from "../style.module.css";
 
+import { ButtonGroup} from '@material-ui/core/';
+
 class Menu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentPage: 1,
+      pizzasPerPage: 3
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
   render() {
+    const indexOfLastPizza = this.state.currentPage * this.state.pizzasPerPage;
+    const indexOfFirstPizza = indexOfLastPizza - this.state.pizzasPerPage;
+    const currentPizzas = this.props.pizzas.slice(indexOfFirstPizza, indexOfLastPizza);
+
+    const renderPizzas = currentPizzas.map((pizza, index) => (<MenuItem key={index} pizza={pizza} />));
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.props.pizzas.length / this.state.pizzasPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (     
+        <li key={number} id={number} onClick={this.handleClick} className={s.page__button}>
+          {number}
+        </li>
+    
+      );
+    });
+
     return (
-      <div>
+      <div className={s.menu__content}>
         <section className={s.slogan}>
           <h1 className={s.slogan__title_3}>Наше меню</h1>
         </section>
         <div className={s.menu}>
-          {this.props.pizzas.map((item) => (<MenuItem key={item.id} pizza={item} />))}
+          {renderPizzas}
+  
         </div>
-        <div className={s.add}>
-          <Link className="button" to="/add">
-            Добавить пиццу
-          </Link>
-        </div>
+        <div className={s.menu__page__buttons}>
+        <ButtonGroup>
+          {renderPageNumbers}
+          </ButtonGroup>
+          </div>
       </div>
     );
   }
 }
+
+
 
 export default Menu;
